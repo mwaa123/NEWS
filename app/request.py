48b20ab3b4f7696b7f7d0b8ec851e128f8dd73b7
ruth <ruthwanjiramugo@gmail.com>
2020-04-26 23:32:35 +0300

    add the method and function from the everything url

--- app/request.py
+++ app/request.py
@@ -39,78 +39,78 @@ def process_results(news_list):
         news_results: A list of news objects
     '''
 
-#     news_results = []
-#     for news_item in news_list:
-#         source = news_item.get('source')
-#         author = news_item.get('author')
-#         title = news_item.get('title')
-#         description = news_item.get('description')
-#         url = news_item.get('url')
-#         urlToImage=news_item.get('urlToImage')
-#         publishedAt =news_item.get('publishedAt')
-#         content = news_item.get('content')
+    news_results = []
+    for news_item in news_list:
+        source = news_item.get('source')
+        author = news_item.get('author')
+        title = news_item.get('title')
+        description = news_item.get('description')
+        url = news_item.get('url')
+        urlToImage=news_item.get('urlToImage')
+        publishedAt =news_item.get('publishedAt')
+        content = news_item.get('content')
 
-#         news_object = News(source,title,author,description,url,urlToImage,publishedAt,content)
+        news_object = News(source,title,author,description,url,urlToImage,publishedAt,content)
 
-#         news_results.append(news_object)
+        news_results.append(news_object)
 
-#     return news_results
-
-
-# Everything = everything.Everything
-
-# # Getting api key
-# api_key = app.config['NEWS_API_KEY']
-# # Getting the news base url
-# evn_url = app.config["EVERYTHING_API_BASE_URL"]
-
-# def get_everything(): #everything
-#     '''
-#     Function that gets the json response to our url request
-#     '''
-#     get_everything_url = evn_url
-
-#     with urllib.request.urlopen(get_everything_url) as url:
-#         get_everything_data = url.read()
-#         get_everything_response = json.loads(get_everything_data)
-
-#         everything_results = None
-
-#         if get_everything_response['articles']:
-#             everything_results_list = get_everything_response['articles']
-#             everything_results = process_results(everything_results_list)
+    return news_results
 
 
-#     return everything_results
+Everything = everything.Everything
 
-# def process_results(everything_list):
-#     '''
-#     Function  that processes the news result and transform them to a list of Objects
+# Getting api key
+api_key = app.config['NEWS_API_KEY']
+# Getting the news base url
+evn_url = app.config["EVERYTHING_API_BASE_URL"]
 
-#     Args:
-#         news_list: A list of dictionaries that contain news details
+def get_everything(): #everything
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_everything_url = evn_url
 
-#     Returns :
-#         news_results: A list of news objects
-#     '''
+    with urllib.request.urlopen(get_everything_url) as url:
+        get_everything_data = url.read()
+        get_everything_response = json.loads(get_everything_data)
 
-#     everything_results = []
-#     for everything_item in everything_list:
-#         id = everything_item['source'].get('id')
-#         name = everything_item["source"].get('name')
-#         # author = news_item.get('author')
-#         title = everything_item.get('title')
-#         description = everything_item.get('description')
-#         url = everything_item.get('url')
-#         urlToImage=everything_item.get('urlToImage')
-#         publishedAt =everything_item.get('publishedAt')
-#         content = everything_item.get('content')
-#         # import pdb; pdb.set_trace()
-#         everything_object = Everything(id,name,title,description,url,urlToImage,publishedAt,content)
+        everything_results = None
 
-#         everything_results.append(everything_object)
+        if get_everything_response['articles']:
+            everything_results_list = get_everything_response['articles']
+            everything_results = process_results(everything_results_list)
 
-#     return everything_results
+
+    return everything_results
+
+def process_results(everything_list):
+    '''
+    Function  that processes the news result and transform them to a list of Objects
+
+    Args:
+        news_list: A list of dictionaries that contain news details
+
+    Returns :
+        news_results: A list of news objects
+    '''
+
+    everything_results = []
+    for everything_item in everything_list:
+        id = everything_item['source'].get('id')
+        name = everything_item["source"].get('name')
+        # author = news_item.get('author')
+        title = everything_item.get('title')
+        description = everything_item.get('description')
+        url = everything_item.get('url')
+        urlToImage=everything_item.get('urlToImage')
+        publishedAt =everything_item.get('publishedAt')
+        content = everything_item.get('content')
+        # import pdb; pdb.set_trace()
+        everything_object = Everything(id,name,title,description,url,urlToImage,publishedAt,content)
+
+        everything_results.append(everything_object)
+
+    return everything_results
 
 
 
