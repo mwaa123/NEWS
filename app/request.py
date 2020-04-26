1e6b8a2dee19c0c8453b8377962cfa2c3b016b7d
ruth <ruthwanjiramugo@gmail.com>
2020-04-26 23:34:12 +0300

    add the methods and functions for the sources url

--- app/request.py
+++ app/request.py
@@ -123,52 +123,52 @@ Sources = sources.Sources
 # Getting api key
 api_key = app.config['NEWS_API_KEY']
 # Getting the news base url
-# source_url = app.config["SOURCES_API_BASE_URL"]
+source_url = app.config["SOURCES_API_BASE_URL"]
 
-# def get_sources(category): #sources
-#     '''
-#     Function that gets the json response to our url request
-#     '''
-#     get_sources_url = source_url.format(category,api_key)
+def get_sources(category): #sources
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_sources_url = source_url.format(category,api_key)
 
-#     with urllib.request.urlopen(get_sources_url) as url:
-#         get_sources_data = url.read()
-#         get_sources_response = json.loads(get_sources_data)
-#         # import pdb; pdb.set_trace()
-#         sources_results = None
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+        # import pdb; pdb.set_trace()
+        sources_results = None
 
-#         if get_sources_response['sources']:
-#             sources_results_list = get_sources_response['sources']
-#             sources_results = process_results(sources_results_list)
+        if get_sources_response['sources']:
+            sources_results_list = get_sources_response['sources']
+            sources_results = process_results(sources_results_list)
 
 
-#     return sources_results
+    return sources_results
 
-# def process_results(sources_list):
-#     '''
-#     Function  that processes the sources result and transform them to a list of Objects
+def process_results(sources_list):
+    '''
+    Function  that processes the sources result and transform them to a list of Objects
 
-#     Args:
-#         sources_list: A list of dictionaries that contain sources details
+    Args:
+        sources_list: A list of dictionaries that contain sources details
 
-#     Returns :
-#         sources_results: A list of sources objects
-#     '''
+    Returns :
+        sources_results: A list of sources objects
+    '''
 
-#     sources_results = []
-#     for sources_item in sources_list:
-#         id = sources_item.get('id')
-#         name = sources_item.get('name')
-#         url = sources_item.get('url')
-#         category =sources_item.get('category')
-#         language = sources_item.get('language')
-#         country = sources_item.get('country')
-#         description = sources_item.get('description')
-#         sources_object = Sources(id,name,url,category,language,country,description)
+    sources_results = []
+    for sources_item in sources_list:
+        id = sources_item.get('id')
+        name = sources_item.get('name')
+        url = sources_item.get('url')
+        category =sources_item.get('category')
+        language = sources_item.get('language')
+        country = sources_item.get('country')
+        description = sources_item.get('description')
+        sources_object = Sources(id,name,url,category,language,country,description)
 
-#         sources_results.append(sources_object)
+        sources_results.append(sources_object)
 
-#     return sources_results
+    return sources_results
 
 
 
