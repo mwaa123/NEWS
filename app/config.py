1dacbeb7655773afa869510fdc32870def5eb7ef
ruth <ruthwanjiramugo@gmail.com>
2020-04-28 16:49:41 +0300

    deployment to heroku

--- app/config.py
+++ app/config.py
@@ -1,3 +1,5 @@
+import os
+
 class Config:
     '''
     General configuration parent class
@@ -5,7 +7,7 @@ class Config:
     NEWS_API_BASE_URL ='https://newsapi.org/v2/top-headlines?category={}&apiKey=5b909bf0340d4286a32ffc2a1dd28dc7'
     EVERYTHING_API_BASE_URL ='https://newsapi.org/v2/everything?q=bitcoin&apiKey=5b909bf0340d4286a32ffc2a1dd28dc7'
     SOURCES_API_BASE_URL='https://newsapi.org/v2/sources?country=us&category={}&apiKey=5b909bf0340d4286a32ffc2a1dd28dc7'
-    # SECRET_KEY = os.environ.get('SECRET_KEY')
+    NEWS_API_KEY =os.environ.get('NEWS_API_KEY')
 class ProdConfig(Config):
     '''
     Production  configuration child class
